678bcedede68fdec7940dee31830894d46c28c8b
Charlotte Swasey <charlotte.swasey@gmail.com>
2024-10-28 21:34:20 +0000

    try to make defaults sensible

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,12 +56,28 @@ def get_json_data(state_ref):
     state_df['time'] = pd.to_datetime(state_df['timestamp'], format='%Y-%m-%dT%H:%M:%S').dt.time
     state_df['date'] = pd.to_datetime(state_df['timestamp'], format='%Y-%m-%dT%H:%M:%S').dt.date
     state_df['full_time'] = state_df['timestamp']
+
+
    
+    min_row = state_df[state_df['votes']==state_df['votes'].min()]
+    min_row['timestamp'] = pd.to_datetime('2020-11-03 00:06:36', format='%Y-%m-%d %H:%M:%S').tz_localize('US/Eastern')
+    min_row['time'] = pd.to_datetime(min_row['timestamp'], format='%Y-%m-%dT%H:%M:%S').dt.time
+    min_row['date'] = pd.to_datetime(min_row['timestamp'], format='%Y-%m-%dT%H:%M:%S').dt.date
+    min_row['full_time'] = min_row['timestamp']
+    min_row['votes'] = 0 
+    min_row['biden_2way'] = 0.5
+    min_row['bidenj'] = 0.5
+    min_row['trumpd'] = 0.5
+    min_row['closeness'] = 'No Data'
 
+    min_row_2 = min_row
+    min_row_2['timestamp'] = pd.to_datetime('2020-11-03 00:08:36', format='%Y-%m-%d %H:%M:%S').tz_localize('US/Eastern')
+    min_row_2['time'] = pd.to_datetime(min_row_2['timestamp'], format='%Y-%m-%dT%H:%M:%S').dt.time
+    min_row_2['date'] = pd.to_datetime(min_row_2['timestamp'], format='%Y-%m-%dT%H:%M:%S').dt.date
+    min_row_2['full_time'] = min_row_2['timestamp']
 
-
-
-    #state_df = pd.concat([state_df, pd.DataFrame(nodat_list)], ignore_index=False)
+    state_df = pd.concat([state_df, min_row], ignore_index=True)
+    state_df = pd.concat([state_df, min_row_2], ignore_index=True)
 ##ADD BEFORE-EVERYTHING DATA POINT WITH "NO DATA"
     state_df = state_df.set_index('full_time')
     state_df.sort_index(inplace=True)
@@ -78,9 +94,9 @@ def read_liveblog_data():
 
     return PA_liveblog
 
-def filter_to_current(data, timestamp):
+def filter_to_current(data, timestamp ):
     timezone = pytz.timezone('America/New_York')
-    dt = timezone.localize(pd.to_datetime("2020-11-3 01:00:00"))
+    dt = timezone.localize(pd.to_datetime("2020-11-2 01:00:00"))
 
     new_df = data.loc[dt:timestamp]
 
@@ -89,15 +105,15 @@ def filter_to_current(data, timestamp):
 def plot_raw_data(dat):
     global fig
     fig = go.Figure()
-    fig.add_trace(go.Scatter(x=dat['timestamp'], y=dat['bidenj'], name='Biden', mode='lines+markers'))
-    fig.add_trace(go.Scatter(x=dat['timestamp'], y=dat['trumpd'], name='Trump', mode='lines+markers', line=dict(color="#FF0000")))
+    fig.add_trace(go.Scatter(x=dat['timestamp'], y=dat['bidenj'], name='Biden', mode='lines'))
+    fig.add_trace(go.Scatter(x=dat['timestamp'], y=dat['trumpd'], name='Trump', mode='lines', line=dict(color="#FF0000")))
     fig.layout.update(title_text="Ballot Flow", 
                 xaxis=dict(
         autorange=False,
-        range=["2020-11-3 01:00:00", "2020-11-6 21:23:22.6871"],
+        range=["2020-11-3 05:00:00", "2020-11-5 21:23:22.6871"],
         type="date"
     ))
-    fig.add_vline(x=date_in[0], line_dash = "dash", line_color = "white")
+    fig.add_vline(x=date_in, line_dash = "dash", line_color = "white")
     ### Add markings for polls close ET/PST/etc?
     st.plotly_chart(fig, use_container_width=True)
 
@@ -130,13 +146,11 @@ option = left.selectbox(
   'Select State',
     ('AZ', 'GA', 'MI', 'NC', 'PA','WI'))
 
-start_date = datetime.datetime(2020, 11, 3)
-end_date = start_date + datetime.timedelta(days=4)
-
+start_date = datetime.datetime(2020, 11, 3) + datetime.timedelta(hours=6)
+end_date = start_date + datetime.timedelta(days=2)
 current_time = pd.Timestamp.now()
 current_hour = current_time.to_pydatetime().hour
 current_time = start_date + pd.to_timedelta(current_hour, unit='h')
-
 
 
 left2, right2 = st.columns([0.7, 0.3], vertical_alignment="top")
@@ -163,7 +177,8 @@ state_curr = filter_to_current(data = state_df, timestamp= dt)
 
 iloc_idx = state_df.index.get_indexer([dt], method='ffill')  # returns absolute index into df e.g. array([5])
 loc_idx = state_df.index[iloc_idx]                             # if you want named index
-my_val = state_df.loc[loc_idx]   
+my_val = state_df.loc[loc_idx]
+
 
 
 PA_liveblog = read_liveblog_data()
@@ -176,25 +191,24 @@ loc_idx = PA_liveblog.index[iloc_idx]                             # if you want 
 liveblog_val = PA_liveblog.loc[loc_idx]  
 
 
-date_in = my_val['timestamp']
+date_in = my_val['timestamp'].iloc[0]
 votes_in = my_val['votes']
 eevp_in = my_val['eevp']
 biden_in = my_val['bidenj']
 biden_hold = my_val['bidenj']
-status = my_val['closeness'][0]
+status = my_val['closeness'].iloc[0]
 max_votes = state_df['votes'].max()
-curr_percent = round(votes_in[0] / max_votes, 3)
-
+curr_percent = round(votes_in.iloc[0] / max_votes, 3)
 
 
 col1, col2 = st.columns([0.7, 0.3])
 with col1:
     plot_raw_data(state_curr)
 with col2:
-    st.write("The last update was given at", date_in[0].strftime("%I:%M%p"), "and currently there is a", status)
-    st.write(str(votes_in[0]), " votes are currently counted")
+    st.write("The last update was given at", date_in.strftime("%I:%M%p"), "and currently there is a", status)
+    st.write(str(votes_in.iloc[0]), " votes are currently counted")
     st.write("This is", str(curr_percent), " percent out of the", str(max_votes), "votes that will eventually be counted in", option)
-    st.write("What did the NYT have to say right now? According to ", liveblog_val['author'][0], ":", liveblog_val['text_update'][0])
+    st.write("What did the NYT have to say right now? According to ", liveblog_val['author'].iloc[0], ":", liveblog_val['text_update'].iloc[0])
 
 
 
